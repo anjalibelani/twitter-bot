3a7338796f4a69f8bbe576531f0660425abc3972
anjalibelani <belani.anjali@gmail.com>
2018-09-11 01:39:22 -0500

    Checking in code for twitter bot

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: 'jYveyVb1ocCcGm3Ix475A9pQR',
+  consumer_secret: 'bC8J9zITixRJk9JyDsIyOf6JUTnCTywtFnZeT6Hb9XzvKmjs9x',
+  access_token_key: '58150818-opGrlcDbiFSzcE17MwkFWj5poATClRG38XfEDaHh1',
+  access_token_secret: 'Cmm9sGwsEIOuH4O7lYVMc5SJPOkLeWtiB6Zs6wLBt5EUj'
+}
